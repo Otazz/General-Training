b991a2bb857b6c160280e77113e17210caffab46
Otávio Rodrigues de Oliveira <ot.rodrigues@hotmail.com>
2018-09-17 15:02:31 -0300

    Delete views.py

--- views.py
+++
@@ -1,7 +0,0 @@
-from aiohttp import web
-import aiohttp_jinja2
-
-async def index(request):
-	return aiohttp_jinja2.render_template('main.html',
-                                              request,
-                                              {})
